fdc994a6d0afce9fee3ae0a995f86ff881844da0
Bee-jay <bjpaiz@gmail.com>
2023-11-07 22:10:29 +0000

    Add basic Prisma models for Message, Channel, DirectMessage, and DirectMessageChat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,6 +13,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>twIRC</title>
+        <meta name="description" content="mIRC clone for the oldheads" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );

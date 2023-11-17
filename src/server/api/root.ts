f452144346e85df0e66c42cc1dbba11ae453603c
Bee-jay <bjpaiz@gmail.com>
2023-11-17 23:51:53 +0000

    Add channels router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { channelsRouter } from "~/server/api/routers/channels";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  channels: channelsRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/channels.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const channelsRouter = createTRPCRouter({
+  // create: protectedProcedure
+  //   .input(z.object({ name: z.string().min(1) }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     // simulate a slow db call
+  //     await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  //     return ctx.db.channel.create({
+  //       data: {
+  //         name: input.name,
+  //         ownerId: ctx.user!.id,
+
+  //       },
+  //     });
+  //   }),
+
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.db.channel.findMany();
+  }),
+
+  getLatest: protectedProcedure.query(({ ctx }) => {
+    return ctx.db.channel.findFirst({
+      orderBy: { createdAt: "desc" },
+    });
+  }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
